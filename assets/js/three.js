5c88c5a812b984de7a874f6a20d57d384def4fe1
Hudson <102180082+ArrowVark@users.noreply.github.com>
2024-12-20 16:24:07 +0000

    Testing threejs stuff

--- assets/js/three.js
+++ assets/js/three.js
@@ -3,9 +3,14 @@ import * as THREE from 'three';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+var myCanvas = document.getElementById("myCanvas");
+
+const renderer = new THREE.WebGLRenderer({
+    // canvas: myCanvas
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+// document.getElementById("canvas-div").appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
